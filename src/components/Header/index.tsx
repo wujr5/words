c375adeec7ae894abcd36c95fb96d1750b0eae0d
吴家荣 <973430584@qq.com>
2021-03-17 19:37:03 +0800

    更新标题为words learner

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ const Header: React.FC = ({ children }) => {
       <div className="w-full flex items-center justify-between">
         <NavLink className="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" to="/">
           <FontAwesomeIcon icon={['far', 'keyboard']} className="h-10 fill-current text-indigo-700 pr-2" />
-          Qwerty Learner
+          Words Learner
         </NavLink>
         <div className="flex bg-white dark:bg-gray-800 transition-colors duration-300 justify-end content-center items-center space-x-3 card rounded-large w-auto on element p-4">
           {children}
